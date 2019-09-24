f3e9ab228f32a7ca97b6e717dd57edcd63a20359
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-09-24 03:22:51 +0000

    Function that prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * times_table - writes the 9 times table starting with 0
+ *
+ * Return: All times table.
+ */
+void times_table(void)
+{
+	int a = 0, b, res;
+
+	while (a <= 5)
+	{
+		b = 0;
+		while (b <= 9)
+		{
+			res = a * b;
+			_putchar(res + '0');
+			if (b != 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+	_putchar('\n');
+}
