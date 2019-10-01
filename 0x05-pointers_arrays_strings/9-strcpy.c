6600ecd0a227dfa78dcfd650cc2c3ecdeccffe08
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-01 21:17:48 +0000

    Function that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *_strcpy - copies the string
+ * @dest: buffer pointed to char
+ * @src: string pointed to src
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
