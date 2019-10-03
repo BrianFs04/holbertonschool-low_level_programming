c25fa4f71b6e6207fc151f9d5f4b1cced539f783
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-03 22:52:35 +0000

    Function that capitalizes certains letters depending on their position

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * cap_string - reverses the content of an array of integers
+ * @c: Pointer to a char
+ *
+ * Return: Uppercase letters
+ */
+char *cap_string(char *c)
+{
+	char req[] = {'\t', ' ', '\n', ',', ';', '.', '!', '?', '"', '(', ')'
+		      , '{', '}'};
+	int i = 0, n;
+
+	while (c[i] != '\0')
+	{
+		n = 0;
+		while (req[n] != '\0')
+		{
+			if (c[i] == req[n] && c[i + 1] >= 97 && c[i + 1] <= 122)
+			{
+				c[i + 1] -= 32;
+			}
+			n++;
+		}
+		i++;
+	}
+	return (c);
+}
