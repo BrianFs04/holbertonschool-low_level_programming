6b101c0b05ff3922a049348f03f9a4270773fef7
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-03 20:02:11 +0000

    Function that compares two strings, then it substracts

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strncat - concatenates two strings
+ * _strcmp - concatenates two strings
  * @s1: Pointer to a char 1
  * @s2: Pointer to a char 2
  *
@@ -8,13 +8,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while(( *s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if(*s1 == *s2)
-		return 0;
+	if (*s1 == *s2)
+		return (0);
 	else
-		return *s1 - *s2;
+		return (*s1 - *s2);
 }
