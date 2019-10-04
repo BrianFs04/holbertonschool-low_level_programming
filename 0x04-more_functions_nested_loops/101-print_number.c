df897a84c00e424e1b5848a5cf5416ca2af425ec
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-04 20:27:10 +0000

    Function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ * @n: number in main
+ */
+void print_number(int n)
+{
+	unsigned int a = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		a = -a;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar(a % 10 + '0');
+}
