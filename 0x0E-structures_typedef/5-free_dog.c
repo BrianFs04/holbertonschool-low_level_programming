221e923a57e39194729031fbb082133a0fe65e2f
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-21 21:14:04 +0000

    Function that frees a structure

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,13 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * free_dog - frees a structure
+ * @d: structure
+ */
 void free_dog(dog_t *d)
 {
-	if (d != 0)
+	if (d)
 	{
 		free(d->name);
 		free(d->owner);
