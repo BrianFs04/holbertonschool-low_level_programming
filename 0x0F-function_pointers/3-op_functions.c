3357cdfc99c14391808a7fd4903c76f1d15ed695
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-23 21:55:02 +0000

    [Feature]: Function that contains the 5 required operations 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds
+ * @a: first integer
+ * @b: second integer
+ * Return: The addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substraction
+ * @a: first integer
+ * @b: second integer
+ * Return: The substraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: first integer
+ * @b: second integer
+ * Return: The multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: first integer
+ * @b: second integer
+ * Return: The division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: first integer
+ * @b: second integer
+ * Return: The modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
