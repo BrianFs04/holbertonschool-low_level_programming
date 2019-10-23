8b699292b5c8d2ff5e2dcbff1d9816a4478d3553
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-23 17:34:44 +0000

    [Feature]: Function pointer that searches for an integer 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - function pointer that searches for an integer
+ * @array:
+ * @size:
+ * @cmp:
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array != NULL && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) == 1)
+				return (i);
+		}
+	}
+	return (-1);
+}
