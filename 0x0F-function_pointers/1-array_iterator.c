5edf2f66690e824a770485b604b0f019e952a09d
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-23 16:38:38 +0000

    [Feature]: Function pointer to print each element of the array 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function on each element
+ * @array: elements
+ * @size: size of the array
+ * @action: function pointers
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL || action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
