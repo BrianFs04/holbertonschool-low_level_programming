f461785c770314d4b1f8f8058ceacde40b5cb0bc
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-31 23:15:11 +0000

    [Feature]: Function that is executed before the main function 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,9 @@
  * myStartupFun - Prints a string
  * before the main
  */
-void myStartupFun (void) __attribute__ ((constructor));
-void myStartupFun (void)
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
 {
-	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
