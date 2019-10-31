637b2964e6c84c2f0be7742d45276ef5a02f0c7c
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-10-31 18:46:20 +0000

    [Fix]: Deleting a condition 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	char *new_str = strdup(str);
 	list_t *new = malloc(sizeof(list_t));
 
-	if (new == NULL && str == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	for (i = 0; new_str[i] != '\0'; i++)
@@ -20,10 +20,8 @@ list_t *add_node(list_t **head, const char *str)
 	new->len = i;
 	new->str = new_str;
 	if (head != NULL)
-	{
 		new->next = *head;
-		*head = new;
-	}
+	*head = new;
 
 	return (new);
 }
