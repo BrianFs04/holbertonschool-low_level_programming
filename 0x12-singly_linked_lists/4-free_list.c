5be53df6a4d1b4c3acfe93ecbe8cd1c52d23f35d
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-01 00:06:49 +0000

    [Feature]: Function that frees a list 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free(head->str);
+		free_list(head->next);
+		free(head);
+	}
+}
