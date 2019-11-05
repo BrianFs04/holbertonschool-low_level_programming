2df8b1ccd6d44a947cb7eb35273b67feeb9884c0
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-05 14:25:36 +0000

    [Fix]: Betty style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,6 +3,7 @@
 /**
  * get_nodeint_at_index - the nth node of a list
  * @head: initialization point
+ * @index: position of the element to print
  * Return: a specific node
  */
 
@@ -10,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for(i = 0; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (head != NULL)
 			head = head->next;
