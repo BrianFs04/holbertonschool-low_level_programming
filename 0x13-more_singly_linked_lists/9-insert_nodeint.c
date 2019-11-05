68508e6e6d8b79b21245aa3d1c9a902f880a90ad
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-05 18:45:30 +0000

    [Fix]: Changing comparaison of idx in the condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (*head == NULL)
-		return (NULL);
-
-	if (idx == 1)
+	if (idx == 0)
 	{
 		*head = new->next;
 		*head = new;
