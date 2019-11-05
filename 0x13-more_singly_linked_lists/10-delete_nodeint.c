3b6444919115e3a2520c0e6c60875bca812eab3c
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-05 16:30:53 +0000

    [Fix]: A condition

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 	}
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (temp == NULL || temp->next == NULL)
 		return (-1);
 
 	curr = temp->next->next;
