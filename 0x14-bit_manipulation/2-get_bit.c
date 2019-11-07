b31a1faeab5e96f08c4c71dd28e0d33cdb143d1f
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-07 22:47:13 +0000

    [Fix]: Betty style

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int i;
 
 	if (index > 64)
-		return(-1);
+		return (-1);
 
 	for (i = 0; i < index; i++)
 	{
 		n = (n >> 1);
 	}
-	return(n & 1);
+	return (n & 1);
 }
