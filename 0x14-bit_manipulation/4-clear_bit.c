ec15d0ac26ea80c675383136ea15d2f46dcd85ab
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-08 00:18:01 +0000

    [Feature]: Function that sets the value of a bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * clear_bit - sets the value of a bit to 0
+ * @n: address to the element
+ * @index: position
+ * Return: 1 if it works, -1 if it fails
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+
+	if (index > 64)
+		return (-1);
+
+	for (i = 0; i <= index; i++)
+	{
+		*n = (*n & (~1 << index));
+	}
+	return (1);
+}
