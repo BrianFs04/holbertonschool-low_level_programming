8aaeaa59aa7d50501e01aea01b586a239f0366b8
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-11-12 23:06:42 +0000

    [Update]: Adding a prototype

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,5 +13,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_file(char *file_from, char *file_to);
+int main(int argc, char *argv[]);
 
 #endif /*HOLBERTON_H*/
