e2b13e49a7c42f4268d278f6970d2d06fc4caf74
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-14 20:41:16 +0000

    [Delete]: Condition if header is NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr;
 	size_t i = 0;
 
-	if (new == NULL || *h == NULL)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
