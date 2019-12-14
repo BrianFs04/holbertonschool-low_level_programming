a3fbaef6caedcbcb2d383f792664d0ebab273a36
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-14 21:29:25 +0000

    [Delete]: Condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,10 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr;
 	size_t i = 0;
 
-	if (h == NULL)
-		return (NULL);
-
-	if (idx > i)
+	if (!h)
 		return (NULL);
 
 	if (new == NULL)
