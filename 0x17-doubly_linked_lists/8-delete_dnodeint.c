75b024aa9529118e263c8c1a6807af059eec384f
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-14 19:00:08 +0000

    [Feature]: Function that deletes the node at index index of a doubly linked list

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete x node in a doubly linked list
+ * @head: double pointer to the first node
+ * @index: x node's position
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *curr;
+	dlistint_t *tmp = *head;
+	size_t i = 0;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (-1);
+
+	else if (index == 0)
+	{
+		*head = tmp->next;
+		tmp->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		while (i < (index - 1) && tmp != NULL)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+
+		curr = tmp->next->next;
+		free(tmp->next);
+		tmp->next = curr;
+		curr->prev = tmp;
+		return (1);
+	}
+
+	return (1);
+}
