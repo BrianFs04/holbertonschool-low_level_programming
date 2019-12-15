338da2e82f06077170f9b4b23f7441f07ca53db7
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-15 04:28:08 +0000

    [Add]: Condition in which the number of elements passed are higher than the number of nodes

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = tmp->next;
+		(*head) = tmp->next;
 		if (*head != NULL)
 			(*head)->prev = NULL;
 		free(tmp);
@@ -26,6 +26,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	while (i < index)
 	{
 		tmp = tmp->next;
+		if (tmp != NULL)
+			return (-1);
 		i++;
 	}
 	if (tmp->next == NULL)
