1fc4c569f6e3db02d69285590a49f04ed7449e30
BrianFs04 <brayanflorezsanabria@gmail.com>
2019-12-16 22:52:33 +0000

    [Fix]: Betty style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,44 +8,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        dlistint_t *new = malloc(sizeof(dlistint_t));
-        dlistint_t *tmp = *h;
-        dlistint_t *curr;
-        size_t i = 0;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+	dlistint_t *curr;
+	size_t i = 0;
 
-        if (new == NULL || h == NULL)
-                return (NULL);
+	if (new == NULL || h == NULL)
+		return (NULL);
 
-        new->n = n;
+	new->n = n;
 
-        if (idx == 0)
-        {
-                new->prev = NULL;
-                if (*h != NULL)
-                {
-                        new->next = *h;
-                        (*h)->prev = new;
-                }
-                *h = new;
-                return (new);
-        }
-        while (i < (idx - 1))
-        {
-                curr = tmp, tmp = tmp->next;
-                if (tmp == NULL)
-                {
-                        free(new);
-                        return (NULL);
-                }
-                i++;
-        }
-        if (tmp->next == NULL)
-        {
-                tmp->next = new, new->prev = tmp;
-                new->next = NULL;
-                return (new);
-        }
-        new->next = tmp->next, new->prev = tmp;
-        curr = tmp->next, tmp->next = new, curr->prev = new;
-        return (new);
+	if (idx == 0)
+	{
+		new->prev = NULL;
+		if (*h != NULL)
+		{
+			new->next = *h;
+			(*h)->prev = new;
+		}
+		*h = new;
+		return (new);
+	}
+	while (i < (idx - 1))
+	{
+		curr = tmp, tmp = tmp->next;
+		if (tmp == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
+		i++;
+	}
+	if (tmp->next == NULL)
+	{
+		tmp->next = new, new->prev = tmp;
+		new->next = NULL;
+		return (new);
+	}
+	new->next = tmp->next, new->prev = tmp;
+	curr = tmp->next, tmp->next = new, curr->prev = new;
+	return (new);
 }
