bf399ca6e0430d8dad72e12ba635774c57fc10a5
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-18 20:33:26 +0000

    [Fix]: Betty style

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		while (tmp != NULL)
 		{
 			printf("%s'%s': '%s'", spa, tmp->key, tmp->value);
-			spa= ", ";
+			spa = ", ";
 			tmp = tmp->next;
 		}
 	}
