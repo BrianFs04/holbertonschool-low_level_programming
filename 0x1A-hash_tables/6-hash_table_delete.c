900ac245a5805c2919adb0fad0e2046384cffa94
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-01-18 21:01:32 +0000

    [Fix]: Betty style

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,11 +11,11 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	for(i = 0; i < ht->size; ++i)
+	for (i = 0; i < ht->size; ++i)
 	{
 		if (ht->array[i] != NULL)
 		{
-			while(ht->array[i] != NULL)
+			while (ht->array[i] != NULL)
 			{
 				tmp = ht->array[i]->next;
 				free(ht->array[i]->key);
